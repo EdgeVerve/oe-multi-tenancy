dfcd507ee3813f04ef78aa235d18b3d367095536
Atul <atul_pandit@edgeverve.com>
2018-07-04 13:43:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const logger = require('oe-logger');
 const log = logger('Oe-multi-tenancy');
 const wrapper = require('./lib/wrapper');
-module.exports = function () {
+module.exports = function (app) {
   log.info('Oe-multi-tenancy module loaded');
   wrapper(app);
 };
