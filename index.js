ec94d434591eeddfc1ec3738737c223370436625
atul-github <atul_pandit@yahoo.com>
2019-01-11 12:56:43 +0530

    2.0.0

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ *
+ * ©20168-2019 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
+ * Bangalore, India. All Rights Reserved.
+ *
+ */
+
 const logger = require('oe-logger');
 const log = logger('Oe-multi-tenancy');
 const wrapper = require('./lib/wrapper');
