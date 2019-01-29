32b038acfd583915859e74cb4d7351dd7700571f
Atul Pandit <Atul_Pandit@ad.Infosys.com>
2019-01-29 17:22:29 +0530

    adding context switching ability

--- server/boot/setSeperator.js
+++ server/boot/setSeperator.js
@@ -1,3 +1,12 @@
+/**
+ *
+ * 2018-2019 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
+ * Bangalore, India. All Rights Reserved.
+ *
+ */
+
+// Author : Atul
+// This function sets seperator. By default it is set to '/'
 const logger = require('oe-logger');
 const log = logger('Oe-multi-tenancy');
 const utils = require('../../lib/utils');
